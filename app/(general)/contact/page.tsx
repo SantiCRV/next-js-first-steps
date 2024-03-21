190b4e9ac7abb8a558561d71729f85f699a1a356
Santiago Ramos <sanoxfdark@gmail.com>
2024-03-20 20:19:40 -0500

    first commit

---
+++ app/(general)/contact/page.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'SEO Contact page',
+  description: 'SEO description contact',
+};
+
+const ContactPage = () => {
+  return (
+    <span className="text-7xl">Contact Page</span>
+  )
+}
+
+export default ContactPage;
---
+++ app/(general)/pricing/page.tsx
@@ -0,0 +1,13 @@
+
+export const metadata = {
+  title: 'SEO Pricing page',
+  description: 'SEO description pricing',
+};
+
+const PricingPage = () => {
+  return (
+    <span className="text-7xl">Pricing Page</span>
+  )
+}
+
+export default PricingPage;
